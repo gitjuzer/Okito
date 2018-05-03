d253d545823aa83ea4ebf05382a0cef206a78554
FractalXX <fade@hotmail.hu>
2018-05-03 20:55:57 +0200

    Jelszó titkosítás hozzáadása

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,54 +1,65 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 // Define schema
 var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
-    email: String,
-    password: String
+  email: String,
+  password: String
 });
 
 // Compile model from schema
-var UserModel = mongoose.model('users', UserSchema );
+var UserModel = mongoose.model('users', UserSchema);
 
-router.post('/register', function(req, res) {
-    let email = req.body.email;
-    let pw = req.body.password;
+router.post('/register', function (req, res) {
+  let email = req.body.email;
+  let pw = req.body.password;
 
-    let user = new UserModel({
+  bcrypt.hash(pw, 10, function (err, hash) {
+    if (err) {
+      console.log('Error hashing password');
+    } else {
+      let user = new UserModel({
         email: email,
-        password: pw
-    });
-
-    user.save(function(err) {
-        if(err) {
-            console.log(err);
-        }
-        else {
-            console.log('Insertion of user successful!');
+        password: hash
+      });
+      user.save(function (err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log('Insertion of user successful!');
         }
         res.redirect("/");
-    });
+      });
+    }
+  });
 });
 
-router.post('/login', function(req, res) {
-    UserModel.findOne({ email: req.body.email }).select('email password').exec((err, user) => {
-        if(err) {
-            console.log("authentication error");
+router.post('/login', function (req, res) {
+  UserModel.findOne({
+    email: req.body.email
+  }).select('email password').exec((err, user) => {
+    if (err) {
+      console.log("authentication error");
+    } else if (!user) {
+      res.redirect('/');
+    } else {
+      bcrypt.compare(req.body.password, user.password, function (err, result) {
+        if (result) {
+          res.redirect('/home');
+        } else {
+          res.redirect('/');
         }
-        else if(!user) {
-            res.redirect('/');
-        }
-        else if(user.password == req.body.password) {
-            res.redirect('/home');
-        }
-    });
+      });
+    }
+  });
 });
 
-router.post('/logout', function(req, res) {
-    res.redirect('/');
+router.post('/logout', function (req, res) {
+  res.redirect('/');
 });
 
 module.exports = router;
