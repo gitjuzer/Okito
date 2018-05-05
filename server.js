51a31eafc7e7a311e1c9dc3c6d780784f0979955
FractalXX <fade@hotmail.hu>
2018-05-05 16:01:26 +0200

    start working on token based authentication

--- server.js
+++ server.js
@@ -29,6 +29,24 @@ passport.use(new LocalStrategy({
   usernameField: 'email'
 }, User.authenticate()));
 
+passport.use(new BearerStrategy(
+  function (token, done) {
+    User.findOne({
+      token: token
+    }, function (err, user) {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false);
+      }
+      return done(null, user, {
+        scope: 'all'
+      });
+    });
+  }
+));
+
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.use('/api', api);
--- server/routes/users.js
+++ server/routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const crypto = require('crypto');
 
 module.exports = function (app, passport) {
   router.post('/register', function (req, res) {
@@ -9,25 +10,46 @@ module.exports = function (app, passport) {
     let pw = req.body.password;
 
     const User = mongoose.model('users');
-
-    User.register(new User({
-      username: email
-    }), pw, function (err, usr) {
-      console.log(usr);
-      console.log(err);
+    crypto.randomBytes(32, function (ex, buf) {
+      let token = buf.toString('hex');
+      User.register(new User({
+        username: email,
+        token: token
+      }), pw, function (err, usr) {
+        res.json({
+          auth: true,
+          token: token
+        });
+      });
     });
+
 
   });
 
   router.post('/login', passport.authenticate('local', {
     session: false
   }), function (req, res) {
-    console.log(req.user);
+    crypto.randomBytes(32, function (ex, buf) {
+      let token = buf.toString('hex');
+      res.json({
+        token: token,
+        auth: true
+      }).end();
+    });
+
   });
 
   router.post('/logout', function (req, res) {
     res.redirect('/');
   });
+
+  router.get('/profile',
+    passport.authenticate('bearer', {
+      session: false
+    }),
+    function (req, res) {
+      res.json(req.user);
+    });
 
   app.use("/rest/users", router);
 };
